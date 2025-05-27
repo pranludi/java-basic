e19e9f55973e92e86d02a6e36c9f1643b20df09c
daekwon <pranludi@gmail.com>
2025-05-28 01:18:08 +0900

    접근 제어자

---
+++ src/access/a/AccessData.java
@@ -0,0 +1,32 @@
+package access.a;
+
+public class AccessData {
+
+    public int publicField;
+    int defaultField;
+    private int privateField;
+
+    public void publicMethod() {
+        System.out.println("AccessData.publicMethod : " + publicField);
+    }
+
+    void defaultMethod() {
+        System.out.println("AccessData.defaultMethod : " + defaultField);
+    }
+
+    private void privateMethod() {
+        System.out.println("AccessData.privateMethod : " + privateField);
+    }
+
+    public void innerAccess() {
+        System.out.println("내부 호출");
+
+        publicField = 100;
+        defaultField = 200;
+        privateField = 300;
+
+        publicMethod();
+        defaultMethod();
+        privateMethod();
+    }
+}
